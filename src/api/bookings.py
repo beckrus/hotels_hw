666066440012198875a50ef293b6102641d63286
Pavel K <krivoshein.pavel@gmail.com>
2025-03-24 13:31:44 +0200

    task_13

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,19 +1,30 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request
 
 from repositories.exceptions import ItemNotFoundException
 from src.schemas.bookings import BookingsAddSchema
-from src.api.dependencies import DBDep, UserIdDep
+from src.api.dependencies import DBDep, UserIdDep, UserIdAdminDep
 
 
 router = APIRouter(prefix="/bookings", tags=["Bookings"])
 
 
+
 @router.get("/")
+async def get_all_bookings(
+    request: Request, 
+    db: DBDep, 
+    user_id:UserIdAdminDep
+):
+    bookings = await db.bookings.get_all()
+    return {"status": "OK", "data": bookings}
+
+
+@router.get("/me")
 async def get_users_bookings(
     request: Request, 
     db: DBDep, 
-    user_id:UserIdDep):
-
+    user_id:UserIdDep
+):
     bookings = await db.bookings.get_filtered(user_id=user_id)
     return {"status": "OK", "data": bookings}
 
@@ -37,16 +48,27 @@ async def add_booking(
     user_id:UserIdDep,
 ):
     try:
-        res_room = await db.rooms.get_one_by_id(id=data.room_id)
-        quantity = res_room.quantity
-        # await db.bookings.get_filtered(
-        #     room_id=data.room_id, 
-        #     date_from=data.date_from, 
-        #     date_to=data.date_to
-        # ) #add filter for avaialbility
+        room = await db.rooms.get_one_by_id(id=data.room_id)
+        #add filter for avaialbility
     except ItemNotFoundException:
         raise HTTPException(status_code=404, detail="Room not found")
-    price = res_room.price * (data.date_to - data.date_from).days
-    booking = await db.bookings.add(user_id=user_id, price=price,data=data)
+    room_price = room.price * (data.date_to - data.date_from).days
+    booking = await db.bookings.add(user_id=user_id, price=room_price,data=data)
     await db.commit()
     return {"status": "OK", "data": booking}
+
+
+@router.delete("/{booking_id}")
+async def add_booking(
+    request: Request,
+    db: DBDep,
+    booking_id: int,
+    user_id:UserIdDep,
+):
+    try:
+        booking = await db.bookings.get_filtered(id=booking_id, user_id=user_id)
+    except ItemNotFoundException:
+        raise HTTPException(status_code=404, detail="Item not found")
+    await db.bookings.delete(booking_id=booking.id)
+    await db.commit()
+    return {"status": "OK"}
